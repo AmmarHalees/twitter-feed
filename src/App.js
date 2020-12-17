7827e8cc245f04a8f189043c624e51370a206715
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2020-12-17 22:25:13 +0200

    basic components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className='app-container'>
+
+      <header>
+
+
+      </header>
+
+      <main>
+
+
+      </main>
      
     </div>
   );
---
+++ src/Post/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Post.module.css';
+
+const Post = () => {
+    return (
+   
+   <div className={styles.post}>
+
+        hi
+
+    </div>
+      );
+}
+ 
+export default Post;
