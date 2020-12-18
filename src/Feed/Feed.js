ff4ea5dd4c694717d3bd0bbb31c13cea45c37a83
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2020-12-18 22:32:26 +0200

    working on feed

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ControlsItem from '../Post/components/ControlsItem/ControlsItem.js';
 import Post from '../Post/Post.js'
 import styles from './Feed.module.css';
+import postdata from "../data/posts.json";
 
 
 
 const Feed = () => {
+
+  console.log(postdata)
     return (
 
         <div className={styles.feed}>
@@ -16,10 +19,7 @@ const Feed = () => {
 
           </div>
 
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
+          {postdata.data.map(({id, user, description, title, src, comments })=><Post key={id} id={id} user={user} description={description} title={title} src={src} comments={comments} />)}
 
         </div>
 
--- src/Post/Post.js
+++ src/Post/Post.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ControlsItem from './components/ControlsItem/ControlsItem';
 import styles from './Post.module.css';
 
-const Post = () => {
+const Post = ({id, user, description, title, src, comments }) => {
   return (
 
-    <div className={styles.post}>
+    <div className={styles.post} name={id}> 
 
       <div className={`${styles.post_column} ${styles.left}`}>
 
-        <img className={styles.user_image} src='https://pbs.twimg.com/profile_images/1085298427662077952/G7pyO36A_400x400.jpg' alt='Dan' />
+        <img className={styles.user_image} src={user.src} alt='Dan' />
 
       </div>
 
@@ -20,7 +20,7 @@ const Post = () => {
 
           <a href='/' className={styles.name}>
 
-            Dan Louis
+            {user.name}
 
           </a>
 
@@ -39,14 +39,12 @@ const Post = () => {
 
         <div className={styles.description}>
 
-          From Willie Mays to Mamie Johnson, the players and teams in the Negro Leagues changed the game of baseball and paved the way for so many today. Glad to see the
-          @MLB
-          officially recognize them as major leaguers.
+          {description}
         </div>
 
         <div className={styles.media}>
 
-          <img className={styles.post_media} src='https://pbs.twimg.com/media/EpdT5xvXYAEybqv?format=jpg&name=medium' alt='post' />
+          <img className={styles.post_media} src={src} alt='post' />
 
         </div>
 
